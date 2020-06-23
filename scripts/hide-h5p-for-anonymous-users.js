307fc8488af311f38df05b81447b5b441dbeb6f8
Paul Ryan <paul.rar@gmail.com>
2020-06-22 14:21:00 -1000

    Add blurring to more H5P Content Types
    previously only iframe H5P content was blurred for anonymous users if that option was enabled in the plugin settings

--- scripts/hide-h5p-for-anonymous-users.js
+++ scripts/hide-h5p-for-anonymous-users.js
@@ -8,8 +8,15 @@
 	$( document ).ready( function() {
 		// Blur (and prevent interaction with) H5P activities for anonymous users.
 		// Also show a message instructing students to log in to access them.
-		$( 'body.anonymous iframe.h5p-iframe' ).before( function() {
-			const loginUrl = Data.loginUrl + '%23' + $( this ).attr( 'id' );
+		$( '.h5p-iframe-wrapper, .h5p-content', 'body.anonymous' ).prepend( function() {
+			let loginUrl = Data.loginUrl;
+
+			// If the H5P element has a child with an id, append that as a named
+			// ahcnor in the redirect URL in the login URL so we can come back it.
+			$( this ).children( '[id]:first' ).each( function () {
+				loginUrl += '%23' + $( this ).attr( 'id' );
+			} );
+
 			return '<p class="h5p-restricted-message"><a href="' + loginUrl + '">' + Data.msgLogInToComplete + '</a></p>';
 		} );
 	} );
