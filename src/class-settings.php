89e10faaabf56e07811ba28a5d982d60be5641e4
Paul Ryan <paul.rar@gmail.com>
2020-09-17 12:03:47 -1000

    Just use high res banner image on settings page

--- src/class-settings.php
+++ src/class-settings.php
@@ -73,7 +73,7 @@ class Settings extends Singleton {
 		?>
 		<div class="wrap">
 			<div class="banner">
-				<img src="<?php echo esc_attr( plugins_url( 'images/banner-772x250.png', plugin_root() ) ); ?>" srcset="<?php echo esc_attr( plugins_url( 'images/banner-772x250.png', plugin_root() ) ); ?> 1x, <?php echo esc_attr( plugins_url( 'images/banner-1544x500.png', plugin_root() ) ); ?> 2x" alt="Banner image illustrating plugin features" />
+				<img src="<?php echo esc_attr( plugins_url( 'images/banner-1544x500.png', plugin_root() ) ); ?>" alt="Banner image illustrating plugin features" />
 			</div>
 			<h2><?php esc_html_e( 'Settings: Dashboard for Pressbooks and H5P', 'd4ph' ); ?></h2>
 			<p><?php esc_html_e( 'Plugin features', 'd4ph' ); ?>:</p>
